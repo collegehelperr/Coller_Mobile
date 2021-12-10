649c6d87f480e1834bd6fe4db56767ec007113f9
Wildan Asyrof Dewayana <asyrof.wildan@gmail.com>
2021-12-10 15:29:39 +0700

    todolist

--- app/src/main/java/com/co/coller/DashboardActivity.java
+++ app/src/main/java/com/co/coller/DashboardActivity.java
@@ -7,15 +7,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.co.coller.api.api;
+import com.co.coller.api.apiClient;
 import com.co.coller.api.sharedPref;
 import com.co.coller.college.CollegeActivity;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.owl93.dpb.CircularProgressView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -24,6 +35,8 @@ public class DashboardActivity extends AppCompatActivity {
     sharedPref sharedPref;
     CircleImageView fotoProf;
     CardView btnSpinner, btnCollege;
+    CircularProgressView circleGraph;
+    float progress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,7 @@ public class DashboardActivity extends AppCompatActivity {
         fotoProf = (CircleImageView) findViewById(R.id.foto_profil_dashboard);
         btnSpinner = (CardView) findViewById(R.id.wheel_spinner);
         btnCollege = (CardView) findViewById(R.id.college_manage);
+        circleGraph = (CircularProgressView) findViewById(R.id.graph_todolist);
 
         tvNama.setText("Hai, " + sharedPref.getName());
 
@@ -44,6 +58,9 @@ public class DashboardActivity extends AppCompatActivity {
         Log.i("Url image", urlImg);
 
         Glide.with(this).load(urlImg).into(fotoProf);
+
+        circleGraph.setVisibility(View.GONE);
+        getPersentase();
 
         btnProfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +82,36 @@ public class DashboardActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(DashboardActivity.this, CollegeActivity.class));
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+    }
+
+    private void getPersentase() {
+        final String uid = sharedPref.getUid();
+        api api = apiClient.getClient().create(api.class);
+        Call<JsonObject> getPersentase = api.getPersentase(uid);
+
+        getPersentase.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if (response.isSuccessful()){
+                    if (response.body() != null){
+                        try {
+                            JSONObject jObj = new JSONObject(new Gson().toJson(response.body()));
+                            circleGraph.setProgress(Float.parseFloat(jObj.getString("Persentase")));
+                            circleGraph.setVisibility(View.VISIBLE);
+//                            progress = Float.parseFloat(jObj.getString("Persentase"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        //Toast.makeText(DashboardActivity.this, "Persentase " + progress, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+
             }
         });
     }
---
+++ app/src/main/java/com/co/coller/model/circlegraph.java
@@ -0,0 +1,29 @@
+package com.co.coller.model;
+
+public class circlegraph {
+
+    private float code;
+    private float Persentase;
+
+
+    // Getter Methods
+
+    public float getCode() {
+        return code;
+    }
+
+    public float getPersentase() {
+        return Persentase;
+    }
+
+    // Setter Methods
+
+    public void setCode(float code) {
+        this.code = code;
+    }
+
+    public void setPersentase(float Persentase) {
+        this.Persentase = Persentase;
+    }
+
+}
